8d4a5347aab696dda579061fcfd81ac56b9fd670
Boris <shevnin.boris2@yandex.ru>
2023-09-14 14:39:58 +0300

    mobile adaptation, add responsive layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,16 @@
 /** @format */
 
-import { FC } from 'react'
-import { Avatar, Menu, Typography } from 'antd'
+import { FC, useCallback, useEffect, useState } from 'react'
+import { Avatar, Button, Col, Menu, Typography } from 'antd'
 import { Link } from 'react-router-dom'
 import icon from '../assets/images/logo.jpg'
-import { BulbOutlined, FundOutlined, HomeOutlined } from '@ant-design/icons'
+import {
+  BulbOutlined,
+  FundOutlined,
+  HomeOutlined,
+  MenuOutlined,
+  SwapOutlined,
+} from '@ant-design/icons'
 
 const menuItems = [
   {
@@ -14,19 +20,39 @@ const menuItems = [
   },
   {
     key: 'Cryptocurrencies',
-    label: <Link to='/cryptocurrencies'>Cryptocurrencies</Link>,
+    label: <Link to='cryptocurrencies'>Cryptocurrencies</Link>,
     icon: <FundOutlined />,
   },
   {
     key: 'News',
-    label: <Link to='/news'>News</Link>,
+    label: <Link to='news'>News</Link>,
     icon: <BulbOutlined />,
   },
 ]
 
 export const Navbar: FC = () => {
+  const [activeMenu, setActiveMenu] = useState(true)
+  const [screenSize, setScreenSize] = useState<number>(1920)
+  const closeMenu = () => {
+    if (screenSize < 1024) setActiveMenu(false)
+    else return
+  }
+
+  useEffect(() => {
+    setScreenSize(window.innerWidth)
+    const resizeHandler = () => setScreenSize(window.innerWidth)
+    window.addEventListener('resize', resizeHandler)
+    return () => window.removeEventListener('resize', resizeHandler)
+  }, [])
+
+  useEffect(() => {
+    if (screenSize < 1024) {
+      setActiveMenu(false)
+    } else setActiveMenu(true)
+  }, [screenSize])
+
   return (
-    <aside className='fixed h-full w-1/6 shadow-2xl bg-bg-dark'>
+    <aside className='sticky h-52 w-full shadow-2xl bg-bg-dark z-10 lg:w-1/6 lg:h-full lg:fixed'>
       <nav>
         <div className='pt-5 text-center'>
           <Avatar
@@ -41,11 +67,24 @@ export const Navbar: FC = () => {
               <span className='text-red-50'>CryptoStats</span>
             </Link>
           </Typography.Title>
+          {window.innerWidth < 1024 && (
+            <Button
+              onClick={() => setActiveMenu((prevState) => !prevState)}
+              className='text-white'
+            >
+              <Col className='-mt-1'>
+                <MenuOutlined />
+              </Col>
+            </Button>
+          )}
         </div>
-        <Menu
-          items={menuItems}
-          theme={'dark'}
-        />
+        {activeMenu && (
+          <Menu
+            items={menuItems}
+            theme={'dark'}
+            onClick={closeMenu}
+          />
+        )}
       </nav>
     </aside>
   )
